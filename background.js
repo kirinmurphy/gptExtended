e4bf207a694a690bd767a08997a83c1f8223e072
KIRIN MURPHY <kirinmurphy@KIRINs-MacBook-Air.local>
2023-08-27 19:54:00 -0600

    some cleanup

--- background.js
+++ background.js
@@ -1,11 +1,30 @@
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    const authToken = details.requestHeaders.filter(({ name }) => name === 'Authorization')[0].value
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { authToken });
-    });
+function handleBeforeSendHeaders(details) {
+  const authTokenHeader = details.requestHeaders.find(({ name }) => name === 'Authorization');
 
-  },
-  {urls: ["https://chat.openai.com/public-api/conversation_limit"]},
+  if (authTokenHeader) {
+    const authToken = authTokenHeader.value;
+
+    // Check if extension context is still valid
+    if (chrome.runtime.lastError) {
+      console.warn("Extension context invalidated:", chrome.runtime.lastError);
+      return;
+    }
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      if (tabs[0] && tabs[0].id) {
+        chrome.tabs.sendMessage(tabs[0].id, { authToken });
+      }
+    });
+  }
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  handleBeforeSendHeaders,
+  { urls: ["https://chat.openai.com/public-api/conversation_limit"] },
   ["requestHeaders"]
 );
+
+// Remove the event listener when the background script is unloaded
+window.addEventListener("unload", function() {
+  chrome.webRequest.onBeforeSendHeaders.removeListener(handleBeforeSendHeaders);
+});
